af723b23345be922adb6239ce2fddd33a5a21634
DavidColblin <da-vid@hotmail.co.uk>
2013-08-01 13:24:46 +0400

    Created

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// Put all your lovely jQuery / Javascript goodies right down here.
+
+// Cufon
+$(document).ready(function(){
+    Cufon.replace('h1 , h2 , nav ul li a', { fontFamily: 'Lobster' });
+    Cufon.replace('h1 small , h3', { fontFamily: 'Raleway' });
+});
